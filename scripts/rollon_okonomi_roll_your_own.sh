5d347f081dffe75f28339a678f4aec430894a1d8
Fred Schoeneman <fred.schoeneman@gmail.com>
2021-10-23 11:25:55 -0700

    Updates wiki and roro

--- scripts/rollon_okonomi_roll_your_own.sh
+++ scripts/rollon_okonomi_roll_your_own.sh
@@ -5,22 +5,20 @@
   gem build roro.gemspec)
 ( mkdir -p sandbox/roll_your_own &&
   cd sandbox/roll_your_own &&
+  rm -rf *
   rvm install 2.7.3 &&
   rvm use 2.7.3@sandbox --create &&
-  gem install --local ../../roro/roro-0.3.23.gem  &&
-  gem install byebug &&
-  roro --help &&
-<<<<<<< HEAD
-#  sudo docker-compose down &&
-  rm -rf *
-  printf "2\n1\ny\ny\ny\ny\ny\ny\ny$var\n" | roro rollon
-=======
-  sudo docker-compose down &&
-  sudo rm -rf *
-  roro generate:mise
+  gem install --local ../../roro/roro-0.3.24.gem  &&
+  roro --help
+#  roro generate
+#  roro generate -c mariadb nginx pistil stamen
+  roro generate
 #  printf "2\n1\ny\ny\ny\ny\ny\ny\ny$var\n" | roro rollon
-  roro rollon
->>>>>>> v2
+#  sudo rm -rf *
+#  roro generate:mise
+##  printf "2\n1\ny\ny\ny\ny\ny\ny\ny$var\n" | roro rollon
+#  roro rollon
+#>>>>>>> v2
 
 )
 
