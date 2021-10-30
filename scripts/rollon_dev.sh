4655d712a1f492c381b856333e54b2912e18e488
Jane <jane.doe@example.com>
2021-10-29 22:34:47 -0700

    Working on making sure install stuff happens before follow-on actions.

--- scripts/rollon_dev.sh
+++ scripts/rollon_dev.sh
@@ -8,18 +8,20 @@
   rvm use 3.0.2@roro --create &&
   bundle &&
   rake install:local
-  #  gem build roro.gemspec
+    gem build roro.gemspec
 )
 
 ( mkdir -p sandbox/dev &&
   cd sandbox/dev &&
+#  sudo docker-compose down &&
+  cd .. &&
+  sudo rm -rf dev &&
+  mkdir -p dev &&
+  cd dev &&
   rvm use 3.0.2@roro --create &&
   gem install --local ../../roro/roro-*.gem  &&
-#  roro --help &&
-#  sudo docker-compose down &&
-#  sudo rm -rf * &&
-#  sudo rm -rf .* &&
-#  printf "2\n2\n1\n1\na$var\n" | roro rollon
+  roro --help
+#  roro rollon
+  printf "2\n2\n1\n1\ny\na$var\n" | roro rollon
 #  printf ${sequence} | roro rollon
-  roro rollon
 )
