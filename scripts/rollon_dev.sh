c4e9a4ebdc8e932cf363b23f779378709c812616
Jane <jane.doe@example.com>
2021-11-01 06:00:39 -0700

    workign

--- scripts/rollon_dev.sh
+++ scripts/rollon_dev.sh
@@ -2,24 +2,21 @@
 
 (
   cd roro &&
-  git add .
-  git commit -m 'for dev'
   rvm install 3.0.2 &&
   rvm use 3.0.2@roro --create &&
-  bundle &&
-  rake install:local
-    gem build roro.gemspec
+  gem build
 )
 
 ( mkdir -p sandbox/dev &&
   cd sandbox/dev &&
-#  sudo docker-compose down &&
+  sudo docker-compose down &&
+#  docker system prune -af --volumes
   cd .. &&
   sudo rm -rf dev &&
   mkdir -p dev &&
   cd dev &&
   rvm use 3.0.2@roro --create &&
-  gem install --local ../../roro/roro-*.gem  &&
+  gem install --local ../../roro/roro-0.3.29.gem  &&
   roro --help
 #  roro rollon
   printf "2\n2\n1\n1\ny\na$var\n" | roro rollon
