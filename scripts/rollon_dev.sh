ea7daa3eed310b110570eca9bf7fbb480bcff399
Fred Schoeneman <fred.schoeneman@gmail.com>
2021-11-09 15:56:49 -0800

    getsome

--- scripts/rollon_dev.sh
+++ scripts/rollon_dev.sh
@@ -10,7 +10,8 @@
   gem install --local ../../roro/roro-0.3.29.gem  &&
   roro --help &&
 #  sudo docker-compose down &&
-  sudo rm -rf * &&
-  roro rollon
-#  printf "1\n1\na$var\n" | roro rollon
+#  sudo rm -rf ./* &&
+#  sudo rm -rf ./.* &&
+#  roro rollon
+  printf "1\n1\n1\na$var\n" | roro rollon
 )
