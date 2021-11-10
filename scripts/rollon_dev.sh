8f5384618120ef3f916d40c28abff060f4006bd1
Jane <jane.doe@example.com>
2021-11-10 01:11:55 -0800

    sync

--- scripts/rollon_dev.sh
+++ scripts/rollon_dev.sh
@@ -4,14 +4,15 @@
   rvm use 3.0.2@sandbox --create &&
   bundle &&
   gem build roro.gemspec )
-( mkdir -p sandbox/dev &&
+( rm sandbox
+  mkdir -p sandbox/dev &&
   cd sandbox/dev &&
   rvm use 3.0.2@sandbox --create &&
   gem install --local ../../roro/roro-0.3.29.gem  &&
   roro --help &&
 #  sudo docker-compose down &&
-#  sudo rm -rf ./* &&
-#  sudo rm -rf ./.* &&
+#  sudo rm -rf * &&
+#  sudo rm -rf .* &&
 #  roro rollon
   printf "1\n1\n1\na$var\n" | roro rollon
 )
