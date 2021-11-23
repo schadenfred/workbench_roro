428571c2c159579ab4a4c2200576386569e46a5f
Fred Schoeneman <fred.schoeneman@gmail.com>
2021-11-22 16:27:57 -0800

    simplie

---
+++ scripts/rollon-dockerized.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo rm -rf ./* ./.*
+docker system prune --volumes
+(cd ~/work/opensource/gems/workbench_roro/roro && docker-compose build)
+
+docker run \
+-v $PWD:/home/schadenfred \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-u 0 \
+-it roar:latest roro rollon
+
